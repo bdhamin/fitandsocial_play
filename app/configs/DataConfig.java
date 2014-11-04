a6019c1ee097b5d7ccf6247e27c42ad0b9de8fb1
bdhamin <bdhamin@gmail.com>
2014-11-04 18:44:16 +0100

    testing

--- app/configs/DataConfig.java
+++ app/configs/DataConfig.java
@@ -28,7 +28,7 @@ public class DataConfig {
         LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
         entityManagerFactory.setPackagesToScan("models");
         entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
-        entityManagerFactory.setDataSource(dataSource());
+        entityManagerFactory.setDataSource(herokuDataSource());
         entityManagerFactory.afterPropertiesSet();
         return entityManagerFactory.getObject();
     }
@@ -36,19 +36,38 @@ public class DataConfig {
     @Bean
     public PlatformTransactionManager transactionManager() {
         JpaTransactionManager transactionManager = new JpaTransactionManager(entityManagerFactory());
-        transactionManager.setDataSource(dataSource());
+        transactionManager.setDataSource(herokuDataSource());
         transactionManager.setJpaDialect(new HibernateJpaDialect());
         return transactionManager;
     }
 
+//    @Bean
+//    public DataSource dataSource() {
+//        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName(Play.application().configuration().getString("db.default.driver"));
+//        dataSource.setUrl(Play.application().configuration().getString("db.default.url"));
+//        dataSource.setUsername(Play.application().configuration().getString("db.default.user"));
+//        dataSource.setPassword(Play.application().configuration().getString("db.default.password"));
+//        return dataSource;
+//    }
+
     @Bean
-    public DataSource dataSource() {
+    public DataSource herokuDataSource() {
         final DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName(Play.application().configuration().getString("db.default.driver"));
-        dataSource.setUrl(Play.application().configuration().getString("db.default.url"));
-        dataSource.setUsername(Play.application().configuration().getString("db.default.user"));
-        dataSource.setPassword(Play.application().configuration().getString("db.default.password"));
+        dataSource.setUrl("jdbc:postgresql://ec2-54-225-101-199.compute-1.amazonaws.com:5432/d2prsu6ijdje7g");
+        dataSource.setUsername("wxeqpnqwzczfai");
+        dataSource.setPassword("gdfuxaaP69mUDo1jiVQ-CV3bjG");
         return dataSource;
     }
 
 }
+
+
+//<bean class="org.apache.commons.dbcp.BasicDataSource" destroy-method="close" id="dataSource">
+//<property name="driverClassName" value="${database.driverClassName}"/>
+//<property name="url" value="#{ 'jdbc:postgresql://' + @dbUrl.getHost() + ':' + @dbUrl.getPort() + @dbUrl.getPath() }"/>
+//<property name="username" value="#{ @dbUrl.getUserInfo().split(':')[0] }"/>
+//<property name="password" value="#{ @dbUrl.getUserInfo().split(':')[1] }"/>
+//db.heroku.url="postgres://wxeqpnqwzczfai:gdfuxaaP69mUDo1jiVQ-CV3bjG@ec2-54-225-101-199.compute-1.amazonaws.com:5432/d2prsu6ijdje7g"
+//postgres://user:password@hostname:port/dbname
